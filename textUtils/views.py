8f5ce9e23bc876249832b704615babc5bb2b36b3
Rajab <muhammedrajab101@gmail.com>
2021-05-01 22:21:18 +0530

    ] Added new replace feature

--- textUtils/views.py
+++ textUtils/views.py
@@ -16,13 +16,20 @@ def index(req):
         if data.get('removePunc'):
             text = re.sub(r'[^\w\s]','', text)
 
+        if str(data.get('replace_this')):
+            if str(data.get('replace_with_this')):
+                text = text.replace(str(data.get('replace_this')), str(data.get('replace_with_this')))
+            else:
+                pass
+
+        if data.get('removeSpace'):
+            text = text.replace(' ', '')
+            
         if data.get('case'):
             if data.get('case') == 'lower': text = text.lower()
             elif data.get('case') == 'upper': text = text.upper()
             else: pass
-        
-        if data.get('removeSpace'):
-            text = text.replace(' ', '')
+    
 
         text_data = {
             'word_count':len(text.split(' ')),
